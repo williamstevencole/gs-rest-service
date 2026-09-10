0ae471dd806e2c58249ed76ccf516f4dd942f90d
William Steven Cole Paz <141452595+williamstevencole@users.noreply.github.com>
2026-09-10 10:47:56 -0600

    Update greeting template to Spanish with date
    Signed-off-by: William Steven Cole Paz <141452595+williamstevencole@users.noreply.github.com>

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-  private static final String template = "Hello 123, %s!";
+  private static final String template = "Hola Mundo, Prueba Devops, 10-09-2026, %s!";
   private final AtomicLong counter = new AtomicLong();
 
   @GetMapping("/greeting")
